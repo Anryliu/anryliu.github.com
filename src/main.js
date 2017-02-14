b6a53edc9be1e39b49618e95d9335cb890213206
liuyrc <liuyrc@yonyou.com>
2017-02-14 17:59:55 +0800

    VUE resume

---
+++ src/main.js
@@ -0,0 +1,34 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+import Vue from 'vue';
+import Vuex from 'vuex';
+import VueRouter from 'vue-router';
+import App from './app';
+import routes from './routes';
+import storeOption from './store';
+import cHeader from 'components/header';
+
+
+Vue.use(Vuex);
+Vue.use(VueRouter);
+
+// 创建一个路由对象用于管理页面的路由
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes
+});
+
+// 创建一个 store 对象用于管理应用状态
+const store = new Vuex.Store(storeOption);
+
+window.__lendApp__ = new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+});
+window.__lendApp__ = new Vue({
+  el: '#header',
+  render: h => h(cHeader)
+});
---
+++ src/views/anry.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="hello">
+        <c-title :text="title"></c-title>
+        <img class="logo" src="../assets/images/logo.png">
+        <p class="welcome">欢迎使用 vue</p>
+        <h1>anry</h1>
+
+        <a href="/hello"><strong>Hello</strong></a>
+        <div v-html="content"></div>
+    </div>
+</template>
+
+<script>
+
+    import {mapState} from 'vuex';
+    import cTitle from 'components/info';
+
+    export default {
+        data () {
+            return {
+                title: 'Im anry!',
+                content: ''
+            }
+        },
+        methods: {
+            async getContent () {
+                const response = await fetch('/api/anry');
+                this.content = await response.text();
+            }
+        },
+        mounted () {
+            this.$store.commit('MESSAGE', '欢迎使用 vue！');
+            this.getContent();
+        },
+
+        components: {cTitle}
+    }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" rel="stylesheet/scss" scoped>
+    h1 {
+        color: #42b983;
+    }
+
+    .logo {
+        width: 100px;
+        height: 100px;
+        a {
+            color: #42b983;
+            text-decoration: none;
+        }
+    }
+</style>
